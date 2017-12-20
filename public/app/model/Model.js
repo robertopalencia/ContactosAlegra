10d52ad95235867f59ebaae38137d8b4ab8ffcb2
robertopalencia <34280211+robertopalencia@users.noreply.github.com>
2017-12-20 20:31:30 -0300

    Update Model.js

--- public/app/model/Model.js
+++ public/app/model/Model.js
@@ -1,23 +1,53 @@
 Ext.define('Alegra.model.Model', {
 	extend: 'Ext.data.Model',
 	fields: [
-		'id',
-		'name',
-		'identification',
-		{ name: 'address', mapping: 'address.address' },
-		{ name: 'city', mapping: 'address.city' },
-		'email',
-		'phonePrimary',
-		'phoneSecondary',
-		'fax',
-		'mobile',
-		'priceList',
-		'seller',
-		'term',
-		'isClient',
-		'isProvider',
-		'observations',
+	{ name: 'id', type: 'int', persist: false },
+        { name: 'name', type: 'string' },
+        { name: 'identification', type: 'string' },
+        { name: 'address', type:'string', mapping: 'address.address' },
+	{ name: 'city', type:'string', mapping: 'address.city' },
+        { name: 'email', type: 'string' },
+        { name: 'phonePrimary', type: 'string' },
+        { name: 'phoneSecondary', type: 'string' },
+        { name: 'fax', type: 'string' },
+        { name: 'mobile', type: 'string' },
+        { name: 'observations', type: 'string' },
+        { name: 'priceList', type: 'int'},
+        { name: 'seller', type: 'int'},
+        { name: 'term', type: 'int'},
+        { name: 'type', type: 'int' },
+        { name: 'internalContacts', type: 'int' },
+        'isClient',
+	'isProvider'
 	],
-	});
+    
+    validators: {
+        name: [
+            'presence',
+            { type: 'length', max:90 },
+        ],
+        identification: [
+            { type: 'length', max:45 },
+        ],
+        email: [
+            { type: 'length', max:100 },
+        ],
+        phonePrimary: [
+            { type: 'length', max:45 },
+        ],
+        phoneSecondary: [
+            { type: 'length', max:45 },
+        ],
+        fax: [
+            { type: 'length', max:45 },
+        ],
+        mobile: [
+            { type: 'length', max:45 },
+        ],
+        observations: [
+            { type: 'length', max:500 },
+        ],
+    },
+});
 
 	
