9ab1e5544cbdc4aea1b35c1c457030285f3a75be
robertopalencia <34280211+robertopalencia@users.noreply.github.com>
2017-12-20 09:31:28 -0400

    Update Model.js

--- public/app/model/Model.js
+++ public/app/model/Model.js
@@ -1,24 +1,25 @@
 Ext.define('Alegra.model.Model', {
 	extend: 'Ext.data.Model',
 	fields: [
-	{ name: 'id', type: 'int', persist: false },
-        { name: 'name', type: 'string' },
-        { name: 'identificacion', type: 'string' },
-        { name: 'email', type: 'string' },
-        { name: 'phonePrimary', type: 'string' },
-        { name: 'phoneSecundary', type: 'string' },
-        { name: 'fax', type: 'string' },
-        { name: 'mobile', type: 'string' },
-        { name: 'observations', type: 'string' },
-        { name: 'priceList', type: 'int', defaultValue: null },
-        { name: 'seller', type: 'int',  defaultValue: null },
-        { name: 'term', type: 'int', defaultValue: null },
-        { name: 'address', type: 'string' },
-        { name: 'type', type: 'int' },
-        { name: 'internalContacts', type: 'int' }
+		'id',
+		'name',
+		'identification',
+		{ name: 'address', mapping: 'address.address' },
+		{ name: 'city', mapping: 'address.city' },
+		'email',
+		'phonePrimary',
+		'phoneSecondary',
+		'fax',
+		'mobile',
+		'priceList',
+		'seller',
+		'term',
+		'isClient',
+		'isProvider',
+		'observations',
 	],
 	
-	 validators: {
+	validators: {
         name: [
             'presence',
             { type: 'length', max:90 },
