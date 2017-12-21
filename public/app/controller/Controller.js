f97886967631f2451e85421956cf546e83fcf191
robertopalencia <34280211+robertopalencia@users.noreply.github.com>
2017-12-21 12:42:41 -0300

    Update Controller.js

--- public/app/controller/Controller.js
+++ public/app/controller/Controller.js
@@ -95,10 +95,10 @@ Ext.define('Alegra.controller.Controller', {
 		let grid = this.getContactGrid();
 		let records = grid.getSelectionModel().getSelection();
 		let store = this.getStoreStore();
-		let title = records.length > 1 ? 'Eliminar ' + records.length + ' Contactos' : 'Eliminar Contacto';
-		let msg = records.length > 1 ? '¿Estás seguro de que deseas eliminar los ' + records.length + ' contactos seleccionados? .' : '¿Estás seguro de que deseas eliminar el contacto seleccionado?';
+		let title = 'Eliminar ' + records.length + ' Contactos';
+		let msg = '¿Estás seguro de que deseas eliminar los ' + records.length + ' contactos seleccionados?';
 
-		if (records.length > 0) {
+		if (records.length > 1) {
 			Ext.Msg.show({
 				title,
                                 msg,
