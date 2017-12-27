21958671ee58df9b3589aaabae327cecb0bbf50b
robertopalencia <34280211+robertopalencia@users.noreply.github.com>
2017-12-27 15:57:28 -0300

    Update Help.js

--- public/app/view/Help.js
+++ public/app/view/Help.js
@@ -15,12 +15,12 @@ Ext.define('Alegra.view.Help', {
 	
   initComponent: function() {
     this.items = [{
-      				xtype: 'form',
+      		    xtype: 'form',
                     border: true,
                     style: 'background-color: #fff;',
                     columnWidth: 1,
-		            layout: 'column',
-				    items:	[
+		    layout: 'column',
+		    items:	[
                             {
                             columnWidth: 0.33,
                             html: '<div id="ayuda" class="box"><h4>Ayudas paso a paso</h4><ul class="vineta"><li><a href="http://ayuda.alegra.com/hc/es/articles/205798409-C%C3%B3mo-hacer-una-Factura-de-venta-" class="initWalkthru" data-walktrhu-name="createInvoice" data-walkthru-id="17946" target="_blank">Cómo crear una factura de venta</a></li><li><a href="http://ayuda.alegra.com/hc/es/articles/206509365--C%C3%B3mo-crear-una-Factura-de-Proveedor-" class="initWalkthru" data-walktrhu-name="createBill" data-walkthru-id="18401" target="_blank">Cómo crear una factura de proveedor</a></li><li><a href="http://ayuda.alegra.com/hc/es/articles/206509615--C%C3%B3mo-importar-contactos-" class="initWalkthru" data-walktrhu-name="createClient" data-walkthru-id="18066" target="_blank">Cómo importar contactos</a></li><li><a href="http://ayuda.alegra.com/hc/es/articles/206512955--C%C3%B3mo-crear-un-Banco-" class="initWalkthru" data-walktrhu-name="createAccount" data-walkthru-id="18065" target="_blank">Cómo crear una cuenta de Banco</a></li><li><a href="http://ayuda.alegra.com/hc/es/articles/206509105-Agregar-un-pago-a-una-factura" class="initWalkthru" data-walktrhu-name="createInvoicePayment" data-walkthru-id="18475" target="_blank">Cómo registrar un pago a una factura de venta</a></li><li><a href="http://ayuda.alegra.com/hc/es/articles/205798349--C%C3%B3mo-crear-una-Nota-Cr%C3%A9dito-" class="initWalkthru" data-walktrhu-name="createCreditNote" data-walkthru-id="18116" target="_blank">Cómo crear una nota crédito</a></li></ul></div>'
